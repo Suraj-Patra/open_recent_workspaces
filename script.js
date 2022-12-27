d05a4f49f8f2ef05d5e5d5caa1a4a2362173bb06
Suraj Patra <patrasuraj2000@gmail.com>
2022-12-28 00:16:35 +0530

    Arranged the code for better understanding

--- script.js
+++ script.js
@@ -1,72 +1,6 @@
+
 const fs = require('fs');   // To read the file
 const child_process = require('child_process'); // To execute the commands
-
-
-const open_file = () => {
-
-    const path = '/home/suraj/Desktop/open_recent_workspaces/commands.txt'; //path of command file
-
-    fs.readFile(path, (err, data) => {
-        data = data.toString(); // Reading file data
-        data_arr = data.split('\n');    // Converting into array by newline
-
-        command_arr = data_arr.filter((data) => {
-            return (data!=='') && data; // Removing the empty strings from array
-        })
-
-        console.log(command_arr);
-        open_programs(command_arr);
-    })
-}
-
-open_file();
-
-
-const open_programs = (command_arr) => {
-
-    for(let command of command_arr) {
-
-        let commands = form_command(command);
-
-        let workspace_command = define_workspace(commands);
-
-        const full_command = define_command(commands);
-
-        child_process.exec(workspace_command);
-        child_process.exec(full_command);   // Executing the command
-
-    }
-
-}
-
-const form_command = (command) => {
-
-    // Converting the commands by ':'
-    let command_arr = command.split(':');
-
-    // Trimming the space around the commands if there any.
-    command_arr[0] = command_arr[0].trim();
-    command_arr[1] = command_arr[1].trim();
-    command_arr[2] = command_arr[2].trim();
-
-    return command_arr;
-}
-
-
-const define_workspace = (commands) => {
-
-    let [, , workspace] = commands;
-
-//     const title = end_command.substr(end_command.lastIndexOf('/')+1);
-//     console.log('name', title);
-
-    workspace = Number(workspace);
-    const workspace_command = `wmctrl -s ${workspace-1}`;
-
-    console.log(`wmctrl -s ${workspace}`);
-    return workspace_command;
-}
-
 
 
 const define_command = (commands) => {
@@ -91,6 +25,75 @@ const define_command = (commands) => {
 
     return full_command;
 }
+
+
+const define_workspace = (commands) => {
+
+    let [, , workspace] = commands;
+
+//     const title = end_command.substr(end_command.lastIndexOf('/')+1);
+//     console.log('name', title);
+
+    workspace = Number(workspace);
+    const workspace_command = `wmctrl -s ${workspace-1}`;
+
+    console.log(`wmctrl -s ${workspace}`);
+    return workspace_command;
+}
+
+
+const form_command = (command) => {
+
+    // Converting the commands by ':'
+    let command_arr = command.split(':');
+
+    // Trimming the space around the commands if there any.
+    command_arr[0] = command_arr[0].trim();
+    command_arr[1] = command_arr[1].trim();
+    command_arr[2] = command_arr[2].trim();
+
+    return command_arr;
+}
+
+
+const open_programs = (command_arr) => {
+
+    for(let command of command_arr) {
+
+        let commands = form_command(command);
+
+        let workspace_command = define_workspace(commands);
+
+        const full_command = define_command(commands);
+
+        child_process.exec(workspace_command);
+        child_process.exec(full_command);   // Executing the command
+
+    }
+
+}
+
+const open_file = () => {
+
+    const path = '/home/suraj/Desktop/open_recent_workspaces/commands.txt'; //path of command file
+
+    fs.readFile(path, (err, data) => {
+        data = data.toString(); // Reading file data
+        data_arr = data.split('\n');    // Converting into array by newline
+
+        command_arr = data_arr.filter((data) => {
+            return (data!=='') && data; // Removing the empty strings from array
+        })
+
+        console.log(command_arr);
+        open_programs(command_arr);
+    })
+}
+
+
+// Start Here
+open_file();
+
 
 
 
